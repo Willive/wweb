f88f3994c34c3fd4feae349d8ca2e6b196326ca2
William Wherry <williamwherryiv@gmail.com>
2017-11-14 00:47:22 -0500

    testing module

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.SERVER_PORT || 3000
 const message = process.env.MESSAGE || 'Hello World'
-export const wweb = () => {
+export default = () => {
     app.get('/', (req, res) => {
         res.send(message)
     })
     app.listen(port, () => console.log(`Server running on port ${port}`))
-}
+};
